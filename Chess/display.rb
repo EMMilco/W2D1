aa19913f57a4bfd2ac88b3f41cbb4ceb12c1d97f
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-01-22 18:03:17 -0500

    Print board

--- Chess/display.rb
+++ Chess/display.rb
@@ -1,17 +1,19 @@
 require 'colorize'
-require_relative 'cursor', 'board'
+require_relative 'cursor'
+require_relative 'board'
 
 class Display
 
   def initialize(board)
     @board = board
-    cursor = Cursor.new([4,4], board)
+    @cursor = Cursor.new([4,4], board)
   end
 
   def render
-    @grid.each do |row|
+    @board.grid.each do |row|
       puts row.join(" ".colorize(:background => :light_white))
     end
+    @cursor.get_input
   end
 
 end
--- Chess/piece.rb
+++ Chess/piece.rb
@@ -8,33 +8,47 @@ class Piece
     @pos = pos
   end
 
-  def symbol
-    self.class.to_s.first
-  end
-
   def to_s
-    self.class.to_s.first.colorize(@color)
+    symbol.colorize(background: :light_white)
   end
 
 end
 
 
 class Rook < Piece
+  def symbol
+    @color == :white ? "\u2656" : "\u265c"
+  end
 end
 
 class Horse < Piece
+  def symbol
+    @color == :white ? "\u2658" : "\u265e"
+  end
 end
 
 class Bishop < Piece
+  def symbol
+    @color == :white ? "\u2657" : "\u265d"
+  end
 end
 
 class Queen < Piece
+  def symbol
+    @color == :white ? "\u2655" : "\u265b"
+  end
 end
 
 class King < Piece
+  def symbol
+    @color == :white ? "\u2654" : "\u265a"
+  end
 end
 
 class Pawn < Piece
+  def symbol
+    @color == :white ? "\u2659" : "\u265f"
+  end
 end
 
 class NullPiece < Piece
@@ -42,5 +56,8 @@ class NullPiece < Piece
 
   def initialize
 
+  end
+  def symbol
+    @color == :white ? " " : " "
   end
 end
